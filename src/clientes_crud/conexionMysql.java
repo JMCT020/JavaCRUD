823fc00c0269d659af5a1cffd74b1030d0559251
ManuelContreras05 <josemanuelcontrerast018@gmail.com>
2023-10-06 19:29:34 -0500

    Descripcion del cambio

---
+++ src/clientes_crud/conexionMysql.java
@@ -0,0 +1,25 @@
+package clientes_crud;
+/**
+ * @author Manuel
+ */
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+
+public class conexionMysql {
+ 
+    Connection cn;
+    
+    public Connection conectar(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            cn=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/crudclientes","root",""); 
+            System.out.println("CONECTADO");
+        }
+        catch(Exception e)
+        {
+            System.out.println("ERROR" + e);
+        }
+        return cn;
+    }
+}
